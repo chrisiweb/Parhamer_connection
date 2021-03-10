a615317876759c8fe833a7111da038e737374f2d
chrisiweb <c.weberndorfer@gmail.com>
2021-03-10 19:49:30 +0100

    create start dialog window

--- start_window.py
+++ start_window.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
 from create_new_widgets import create_new_gridlayout, create_new_label
 
 
@@ -9,7 +9,11 @@ class Ui_StartWindow(object):
         self.StartWindow = StartWindow
         StartWindow.setObjectName("StartWindow")
         StartWindow.setWindowTitle("Herzlich Willkommen bei LaMA")
-        gridLayout = create_new_gridlayout(self.StartWindow)
+        gridlayout = QtWidgets.QGridLayout()
+
+        gridlayout.setObjectName("gridlayout")
+        # gridlayout = create_new_gridlayout(StartWindow)
+#         gridLayout = create_new_gridlayout(self.StartWindow)
 
         label_1 = create_new_label(self.StartWindow, """
 Herlich Willkommen!
@@ -18,45 +22,42 @@ Es freut uns sehr, dass Sie sich für das Programm LaMA interessieren!
 Um starten zu können, muss LaMA zu Beginn konfiguriert werden. Dazu muss die Aufgabendatenbank heruntergeladen werden.
 
 Möchten Sie die Konfiguration beginnen und die Datenbank herunterladen?
-        """)
+            """)
+        gridlayout.addWidget(label_1, 0,0,1,1)
+        StartWindow.setLayout(gridlayout)
 
-        gridLayout.addWidget(label_1, 0,0,1,1)
 
-    #     self.buttonBox_welcome = QtWidgets.QDialogButtonBox(self.StartWindow)
-    #     self.buttonBox_welcome.setStandardButtons(
-    #         QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
-    #     )
+        self.buttonBox_welcome = QtWidgets.QDialogButtonBox(self.StartWindow)
+        self.buttonBox_welcome.setStandardButtons(
+            QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
+        )
 
-    #     # buttonS = self.buttonBox_titlepage.button(QtWidgets.QDialogButtonBox.Save)
-    #     # buttonS.setText('Speichern')
-    #     buttonX = self.buttonBox_welcome.button(QtWidgets.QDialogButtonBox.Cancel)
-    #     buttonX.setText("Abbrechen")
-    #     self.buttonBox_welcome.setObjectName("buttonBox_variation")
-    #     self.buttonBox_welcome.rejected.connect(self.cancel_pressed)
-    #     self.buttonBox_welcome.accepted.connect(self.start_download)
+        # buttonS = self.buttonBox_titlepage.button(QtWidgets.QDialogButtonBox.Save)
+        # buttonS.setText('Speichern')
+        buttonX = self.buttonBox_welcome.button(QtWidgets.QDialogButtonBox.Cancel)
+        buttonX.setText("Abbrechen")
+        self.buttonBox_welcome.setObjectName("buttonBox_variation")
+        self.buttonBox_welcome.rejected.connect(self.cancel_pressed)
+        self.buttonBox_welcome.accepted.connect(self.start_download)
 
-    #     self.gridLayout.addWidget(self.buttonBox_welcome, 1,0,1,1)
+        gridlayout.addWidget(self.buttonBox_welcome, 1,0,1,1)
 
-    # def cancel_pressed(self):
-    #     print('cancel')
+    def cancel_pressed(self):
+        sys.exit()
 
-    # def start_download(self):
-    #     print('accept')
-    #     # self.Dialog.accept()
+    def start_download(self):
+        self.StartWindow.accept()
 
 app = QApplication(sys.argv)
-StartWindow = QMainWindow()
-# MainWindow.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-# screen_resolution = app.desktop().screenGeometry()
-# screen_width, screen_height = screen_resolution.width(), screen_resolution.height()
 
-# StartWindow.setGeometry(
-#     30, 30, round(screen_width * 0.5), round(screen_height * 0.8)
-# )
-# StartWindow.move(30, 30)
-
+Dialog = QtWidgets.QDialog(
+    None,
+    QtCore.Qt.WindowSystemMenuHint
+    | QtCore.Qt.WindowTitleHint
+    | QtCore.Qt.WindowCloseButtonHint,
+)
 ui = Ui_StartWindow()
-ui.setupUi(StartWindow)
+ui.setupUi(Dialog)
+Dialog.show()
 
-StartWindow.show()
 app.exec()
