0a9ea0ff83025bdb6e5d32b20fb59fba90cfea3c
chrisiweb <c.weberndorfer@gmail.com>
2021-10-14 17:30:49 +0200

    push v3.1.6

--- LaMA.py
+++ LaMA.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #### Version number ###
-__version__ = "v3.1.5"
-__lastupdate__ = "09/21"
+__version__ = "v3.1.6"
+__lastupdate__ = "10/21"
 ##################
 
 print("Loading...")
