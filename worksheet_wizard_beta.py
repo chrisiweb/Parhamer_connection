6a42edbd3f7dfca5f1fe112bd0805470b8271afa
chrisiweb <c.weberndorfer@gmail.com>
2022-02-07 18:43:43 +0100

    edit worksheet_wizard_beta

--- worksheet_wizard_beta.py
+++ worksheet_wizard_beta.py
@@ -1,11 +1,14 @@
 import random
-# import math
-# import os
-# from config_start import path_localappdata_lama, path_programm
+import math
 import decimal
-# import subprocess
-# from tex_minimal import tex_preamble, tex_end
-# from create_pdf import create_pdf, open_pdf_file, build_pdf_file
+from venv import create
+
+from numpy import isin
+import os
+from config_start import path_localappdata_lama, path_programm
+import subprocess
+from tex_minimal import tex_preamble, tex_end
+from create_pdf import  open_pdf_file
 
 
 list_of_topics_wizard = ['Addition', 'Subtraktion']
@@ -13,13 +16,14 @@ list_of_topics_wizard = ['Addition', 'Subtraktion']
 D = decimal.Decimal
 
 
-"""$\begin{array}{l}
+"""\item $\begin{array}{l}
 235 \cdot 123 \\ \hline
-\hspace{-0.5em} 1235 \\
-\enspace 470 \\
-705 \\ \hline
-28905\\
-\end{array}$"""
+\antwortzeile \hspace{-0.5em} 1235\\
+\antwortzeile \enspace 470\\
+\antwortzeile 705 \\ \hline
+\antwortzeile 28905 \\
+\end{array}$
+\antwort[\vspace{2cm}]{}"""
 
 def get_random_number(min, max, decimal=0):
     x = round(random.uniform(min,max),decimal)
@@ -33,24 +37,101 @@ def get_random_number(min, max, decimal=0):
     else:
         return x
 
+
+def normalize_decimal(d):
+    normalized = d.normalize()
+    sign, digit, exponent = normalized.as_tuple()
+    return normalized if exponent <= 0 else normalized.quantize(1)
+
+def change_to_integer(n):
+    if isinstance(n, int):
+        return n
+    else:
+        n = normalize_decimal(n)
+        n = str(n).replace('.','')
+        return int(n)
+
+
+
+def get_number_of_digits(n):
+    return len(str(int(n)))
+
 def split_into_digits(n):
+    # print(n)
+    n = str(n).replace('.','')
+    # print(n)
+    # n=int(n)
+    return [int(d) for d in n]
+
+# x= get_random_number(235,235,0)
+# y= get_random_number(123,123, 0)
+
+# integer = change_to_integer(x)
+
+# _list = split_into_digits(change_to_integer(y))
+
+# result = []
+# for digit in _list:
+#     result.append(int(x*digit))
+
+# factor_digit_length = get_number_of_digits(x)
+
+
+def create_single_line_multiplication(digit):
+    subresult = str(int(x*digit))
+
+    string = '\\antwortzeile {} \\\\\n'.format(subresult)
+    return string
+
+
+content = ""
+for all in range(20):
+    x= get_random_number(10,99,0)
+    y= get_random_number(10,99, 0)
+
+    integer = change_to_integer(x)
+
+    _list = split_into_digits(change_to_integer(y))
+
+    result = []
+    for digit in _list:
+        result.append(int(x*digit))
+
+    factor_digit_length = get_number_of_digits(x)
+
+    content += """\item$\\begin{{array}}{{l}}
+    {0} \cdot {1} \\\\ \hline
+    """.format(str(x).replace('.',','),str(y).replace('.',','))
+
+    for i, digit in enumerate(_list):
+        content += create_single_line_multiplication(digit)
     
-    return [int(d) for d in str(n)]
+    content += '\\antwortzeile {} \\\\\n'.format(x*y)
+    content += "\end{array}$\n\n"
 
-x= get_random_number(235,235,1)
-y= get_random_number(123,123, 0)
+# print(content)
+titel = "Arbeitsblatt"
+nummerierung = "(i)"
+columns = 2
 
-print(x)
-print(y)
+content = """
+\section{{{0}}}
 
-print(x*y)
-_list = split_into_digits(y)
+\\begin{{multicols}}{{{1}}}
+\\begin{{enumerate}}[{2}]
+{3}
+\end{{enumerate}}
+\end{{multicols}}
+""".format(titel, columns, nummerierung, content)
 
-result = []
-for digit in _list:
-    result.append(x*digit)
 
-print(result)
+print(content)
+
+# \hspace{-0.5em} 1235 \\
+# \enspace 470 \\
+# 705 \\ \hline
+# 28905\\
+# \end{array}$"""
 
 # def create_single_example_subtraction(minimum, maximum, commas, negative_solutions_allowed):
 #     x = get_random_number(minimum,maximum, commas)
@@ -137,25 +218,26 @@ print(result)
 
 
 
-# path_file = os.path.join(
-#     path_localappdata_lama, "Teildokument", "worksheet.tex"
-#     )
+
+path_file = os.path.join(
+    path_localappdata_lama, "Teildokument", "worksheet.tex"
+    )
 
 
 
 # content = create_latex_worksheet()
 
-# with open(path_file, "w", encoding="utf8") as file:
-#     file.write(tex_preamble(solution="solution_on"))
+with open(path_file, "w", encoding="utf8") as file:
+    file.write(tex_preamble(solution="solution_on"))
 
-#     file.write(content)
+    file.write(content)
 
-#     file.write(tex_end)
+    file.write(tex_end)
 
-# name = 'Arbeitsblatt'
-# head, tail = os.path.split(name)
-# file_name = tail
-# folder_name = "{0}/Teildokument".format(path_programm)
+name = 'worksheet'
+head, tail = os.path.split(name)
+file_name = tail
+folder_name = "{0}/Teildokument".format(path_programm)
 
 
 # latex_output_file = open(
@@ -168,20 +250,20 @@ print(result)
 # drive_programm = os.path.splitdrive(path_programm)[0]
 # drive_save = os.path.splitdrive(folder_name)[0]
 
-# drive = ""
+drive = ""
 
-# terminal_command = 'cd "{1}" & latex -interaction=nonstopmode --synctex=-1 "{2}.tex" & latex -interaction=nonstopmode --synctex=-1 "{2}.tex" & dvips "{2}.dvi" & ps2pdf -dNOSAFER -dALLOWPSTRANSPARENCY "{2}.ps"'.format(
-#     drive, folder_name, file_name
-# )
-
-
-# process = subprocess.Popen(
-#     terminal_command,
-#     cwd=os.path.splitdrive(path_programm)[0],
-#     shell=True,
-# )
-
-# process.wait()
+terminal_command = 'cd "{1}" & latex -interaction=nonstopmode --synctex=-1 "{2}.tex" & latex -interaction=nonstopmode --synctex=-1 "{2}.tex" & dvips "{2}.dvi" & ps2pdf -dNOSAFER -dALLOWPSTRANSPARENCY "{2}.ps"'.format(
+    drive, folder_name, file_name
+)
 
 
-# open_pdf_file(folder_name, file_name)
+process = subprocess.Popen(
+    terminal_command,
+    cwd=os.path.splitdrive(path_programm)[0],
+    shell=True,
+)
+
+process.wait()
+
+
+open_pdf_file(folder_name, file_name)
