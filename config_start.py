6b306784f41fa239d0d5e5b2a5cc78572e0befa8
chrisiweb <c.weberndorfer@gmail.com>
2022-09-15 18:35:29 +0200

    push v4.0.1

--- config_start.py
+++ config_start.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 #### Version number ###
-__version__ = "v4.0.0"
+__version__ = "v4.0.1"
 
 if sys.platform.startswith("win"):
     ##### NOT IN USE ! (Working!) - Activate when installer is used!
