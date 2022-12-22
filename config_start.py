31bb55d6e1a7128233afa9b15f889d5eacdabafa
chrisiweb <c.weberndorfer@gmail.com>
2022-12-22 18:23:20 +0100

    back to v4.1.5

--- config_start.py
+++ config_start.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 #### Version number ###
-__version__ = "v4.2.0"
+__version__ = "v4.1.5"
 
 if sys.platform.startswith("win"):
     ##### NOT IN USE ! (Working!) - Activate when installer is used!
