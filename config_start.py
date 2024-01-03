be699df2c24efbbc0b99fa3d438ab75d5096151b
chrisiweb <c.weberndorfer@gmail.com>
2024-01-03 11:27:29 +0100

    update to current version

--- config_start.py
+++ config_start.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 #### Version number ###
-__version__ = "v4.5.2"
+__version__ = "v4.6.0"
 
 if sys.platform.startswith("win"):
     programdata = os.getenv('PROGRAMDATA')
