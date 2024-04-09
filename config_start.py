0036d5a9aff29e9dcb73eb10e1bde55f82ab0351
chrisiweb <c.weberndorfer@gmail.com>
2024-04-09 14:55:35 +0200

    push v4.6.1

--- config_start.py
+++ config_start.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 #### Version number ###
-__version__ = "v4.6.0"
+__version__ = "v4.6.1"
 
 if sys.platform.startswith("win"):
     programdata = os.getenv('PROGRAMDATA')
