6978da8de1e415be29e87c32ad24b435adb9f757
chrisiweb <44054474+chrisiweb@users.noreply.github.com>
2025-09-25 12:58:08 +0200

    warning fix

--- create_nonograms.py
+++ create_nonograms.py
@@ -1,18 +1,18 @@
 
 nonogramm_empty = """\\renewcommand{\\arraystretch}{1.2}
 \\begin{tabular}{c|p{0,3cm}|p{0,3cm}|p{0,3cm}|p{0,3cm}|p{0,3cm}|p{0,3cm}|p{0,3cm}|p{0,3cm}|p{0,3cm}|p{0,3cm}|}
-\multicolumn{1}{l}{}&\multicolumn{1}{c}{A}&\multicolumn{1}{c}{B}&\multicolumn{1}{c}{C}&\multicolumn{1}{c}{D}&\multicolumn{1}{c}{E}&\multicolumn{1}{c}{F}&\multicolumn{1}{c}{G}&\multicolumn{1}{c}{H}&\multicolumn{1}{c}{I}&\multicolumn{1}{c}{J}\\\\ \cline{2-11}
-0& A0 & B0 & C0 & D0 & E0 & F0 & G0 & H0 & I0 & J0 \\\\ \cline{2-11}
-1& A1 & B1 & C1 & D1 & E1 & F1 & G1 & H1 & I1 & J1 \\\\ \cline{2-11}
-2& A2 & B2 & C2 & D2 & E2 & F2 & G2 & H2 & I2 & J2 \\\\ \cline{2-11} 
-3& A3 & B3 & C3 & D3 & E3 & F3 & G3 & H3 & I3 & J3 \\\\ \cline{2-11}
-4& A4 & B4 & C4 & D4 & E4 & F4 & G4 & H4 & I4 & J4 \\\\ \cline{2-11}
-5& A5 & B5 & C5 & D5 & E5 & F5 & G5 & H5 & I5 & J5 \\\\ \cline{2-11}
-6& A6 & B6 & C6 & D6 & E6 & F6 & G6 & H6 & I6 & J6 \\\\ \cline{2-11}
-7& A7 & B7 & C7 & D7 & E7 & F7 & G7 & H7 & I7 & J7 \\\\ \cline{2-11} 
-8& A8 & B8 & C8 & D8 & E8 & F8 & G8 & H8 & I8 & J8 \\\\ \cline{2-11} 
-9& A9 & B9 & C9 & D9 & E9 & F9 & G9 & H9 & I9 & J9 \\\\ \cline{2-11}
-\end{tabular}"""
+\\multicolumn{1}{l}{}&\\multicolumn{1}{c}{A}&\\multicolumn{1}{c}{B}&\\multicolumn{1}{c}{C}&\\multicolumn{1}{c}{D}&\\multicolumn{1}{c}{E}&\\multicolumn{1}{c}{F}&\\multicolumn{1}{c}{G}&\\multicolumn{1}{c}{H}&\\multicolumn{1}{c}{I}&\\multicolumn{1}{c}{J}\\\\ \\cline{2-11}
+0& A0 & B0 & C0 & D0 & E0 & F0 & G0 & H0 & I0 & J0 \\\\ \\cline{2-11}
+1& A1 & B1 & C1 & D1 & E1 & F1 & G1 & H1 & I1 & J1 \\\\ \\cline{2-11}
+2& A2 & B2 & C2 & D2 & E2 & F2 & G2 & H2 & I2 & J2 \\\\ \\cline{2-11} 
+3& A3 & B3 & C3 & D3 & E3 & F3 & G3 & H3 & I3 & J3 \\\\ \\cline{2-11}
+4& A4 & B4 & C4 & D4 & E4 & F4 & G4 & H4 & I4 & J4 \\\\ \\cline{2-11}
+5& A5 & B5 & C5 & D5 & E5 & F5 & G5 & H5 & I5 & J5 \\\\ \\cline{2-11}
+6& A6 & B6 & C6 & D6 & E6 & F6 & G6 & H6 & I6 & J6 \\\\ \\cline{2-11}
+7& A7 & B7 & C7 & D7 & E7 & F7 & G7 & H7 & I7 & J7 \\\\ \\cline{2-11} 
+8& A8 & B8 & C8 & D8 & E8 & F8 & G8 & H8 & I8 & J8 \\\\ \\cline{2-11} 
+9& A9 & B9 & C9 & D9 & E9 & F9 & G9 & H9 & I9 & J9 \\\\ \\cline{2-11}
+\\end{tabular}"""
 
 list_all_pixels = [
 'A0', 'B0', 'C0', 'D0', 'E0', 'F0', 'G0', 'H0', 'I0', 'J0',
