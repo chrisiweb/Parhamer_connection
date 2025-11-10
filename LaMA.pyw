ef3d4eeb40c66ced08b6d9dd79c5786355547ad8
chrisiweb <44054474+chrisiweb@users.noreply.github.com>
2025-11-10 16:25:57 +0100

    push v5.0.3

--- LaMA.pyw
+++ LaMA.pyw
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-__lastupdate__ = "10/25"
+__lastupdate__ = "11/25"
 
 #minor change##
 ##################
--- config_start.py
+++ config_start.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 #### Version number ###
-__version__ = "v5.0.2"
+__version__ = "v5.0.3"
 
 
 if sys.platform.startswith("win"):
