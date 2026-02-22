28032691f8632b2da12f81509a29bd68d3fb265f
Christoph <c.weberndorfer@gmail.com>
2026-02-22 15:55:27 +0100

    change macos build

--- _MacOS_build_dmg.sh
+++ _MacOS_build_dmg.sh
@@ -12,8 +12,6 @@ DIST_DIR="dist"
 # === 🧹 Vorherige Builds löschen ===
 echo "🧹 Entferne alte Build-Dateien..."
 # Entfernt alle Schutzattribute
-xattr -cr .
-chmod -R u+w .
 rm -rf build "$DIST_DIR" "$APP_NAME.spec"
 
 
