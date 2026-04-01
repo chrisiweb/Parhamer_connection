8f3c63bb14c9aebab5eae6f9b38c26c11df76309
chrisiweb <44054474+chrisiweb@users.noreply.github.com>
2026-04-01 13:35:35 +0200

    lama_beta update

--- _WIN_Create_LaMA_setup_ONLY EXE.bat
+++ _WIN_Create_LaMA_setup_ONLY EXE.bat
@@ -15,9 +15,6 @@ set "INNO_COMPILER=C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 set "INNO_SCRIPT=C:\Users\cwebe\Desktop\_create_lama_installer\script_create_installer - includetinylatex.iss"
 
 
-set /p APP_VERSION=Bitte Versionsnummer eingeben (z.B. 1.2.3): 
-echo Aktuelle Version:%APP_VERSION%
-
 echo ==============================
 echo Schritt 1: L sche dist & build
 echo ==============================
@@ -27,8 +24,13 @@ if exist build rd /s /q build
 echo ==============================
 echo Schritt 2: Erstelle LaMA.exe mit PyInstaller
 echo ==============================
-"%PYINSTALLER_EXE%" -F -i icon_lama.ico LaMA.pyw
+"%PYINSTALLER_EXE%" -F -i icon_lama.ico --name "LaMA_beta" LaMA.pyw
 if errorlevel 1 (
     echo Fehler beim Erstellen der EXE.
     exit /b 1
 )
+
+echo ==============================
+echo Fertig!
+echo ==============================
+pause
