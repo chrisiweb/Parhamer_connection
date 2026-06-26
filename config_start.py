22425a0face76fcb345fa697be61671cd35e217c
chrisiweb <44054474+chrisiweb@users.noreply.github.com>
2026-06-26 09:32:04 +0200

    downgrad v5.1.3

--- config_start.py
+++ config_start.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 #### Version number ###
-__version__ = "v5.1.4"
+__version__ = "v5.1.3"
 
 def resource_path(*relative_path):
     """ermittelt den Pfad zu Ressourcen im Entwicklermodus UND im Build"""
