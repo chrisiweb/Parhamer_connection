64d32d5a9e39887951ce1bbd31a1eb3d2c73bf5d
chrisiweb <44054474+chrisiweb@users.noreply.github.com>
2026-07-29 12:15:27 +0200

    update create_lama only exe

--- _WIN_Create_LaMA_setup_ONLY EXE.bat
+++ _WIN_Create_LaMA_setup_ONLY EXE.bat
@@ -4,9 +4,10 @@ setlocal
 :: ==============================
 :: Variablen
 :: ==============================
-set "PYINSTALLER_EXE=C:\Users\cwebe\AppData\Local\Programs\Python\Python312\Scripts\pyinstaller.exe"
+set "PYINSTALLER_EXE=C:\Users\cwebe\AppData\Roaming\Python\Python313\Scripts\pyinstaller.exe"
+:: WORKING: C:\Users\cwebe\AppData\Local\Programs\Python\Python312\Scripts\pyinstaller.exe
 :: C:\Users\cwebe\AppData\Roaming\Python\Python313\Scripts\pyinstaller.exe
-set "EXE_NAME=LaMA_beta.exe"
+set "EXE_NAME=LaMA.exe"
 set "BUILD_DIR=dist"
 set "DEST_DIR=C:\Users\cwebe\Desktop\_create_lama_installer"
 set "SETUP_PATH=%DEST_DIR%\lama_installer\LaMA_setup.exe"
@@ -15,8 +16,9 @@ set "INNO_COMPILER=C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 set "INNO_SCRIPT=C:\Users\cwebe\Desktop\_create_lama_installer\script_create_installer - includetinylatex.iss"
 
 
+
 echo ==============================
-echo Schritt 1: L sche dist & build
+echo Schritt 1: L�sche dist & build
 echo ==============================
 if exist "%BUILD_DIR%" rd /s /q "%BUILD_DIR%"
 if exist build rd /s /q build
@@ -24,7 +26,7 @@ if exist build rd /s /q build
 echo ==============================
 echo Schritt 2: Erstelle LaMA.exe mit PyInstaller
 echo ==============================
-"%PYINSTALLER_EXE%" -F -i icon_lama.ico --name "LaMA_beta" LaMA.pyw
+"%PYINSTALLER_EXE%" -F -i icon_lama.ico LaMA.pyw
 if errorlevel 1 (
     echo Fehler beim Erstellen der EXE.
     exit /b 1
